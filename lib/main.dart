c601bf1263065b3feca557b50b51eefd952e820b
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-11-09 09:33:46 -0300

    add card of transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
+
 
 main() => runApp(ExpensesApp());
 
@@ -17,20 +17,11 @@ class ExpensesApp extends StatelessWidget {
 
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
+
+  
+
+
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +30,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -49,14 +39,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr){
-              return Card(
-                child: Text(tr.title),
-                
-              );
-            }).toList(),
-          )
+          TransactionUser(),
         ],)
     );
   }
