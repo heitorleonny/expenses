f5ec34ed2f58bb58fb2dff69032cd58188fa85ed
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-11-10 12:19:37 -0300

    add changes to form submission

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -8,44 +8,51 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+  if(title.isEmpty || value <= 0){
+    return;
+  }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(children: <Widget>[
-                TextField(
-                 controller: titleController,
-                  decoration: InputDecoration(
-                    labelText: 'Título'
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(children: <Widget>[
+          TextField(
+            controller: titleController,
+            onSubmitted: (_) => _submitForm(),
+            decoration: InputDecoration(labelText: 'Título'),
+          ),
+          TextField(
+            controller: valueController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            onSubmitted: (_) => _submitForm(),
+            decoration: InputDecoration(labelText: 'Valor (R\$)'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: Text(
+                  'Nova Transação',
+                  style: TextStyle(
+                    color: Colors.purple,
                   ),
                 ),
-                TextField(
-                  controller: valueController,
-                  decoration: InputDecoration(
-                    labelText: 'Valor (R\$)'
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      child: Text('Nova Transação',
-                      style: TextStyle(
-                        color: Colors.purple,
-                      ),),
-                    
-                      onPressed: () {
-                        final title = titleController.text;
-                        final value = double.tryParse(valueController.text) ?? 0.0;
-                        onSubmit(title, value);
-                      },
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          );
+                onPressed: _submitForm,
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
   }
 }
