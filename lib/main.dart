8078f955077037613316c237883c167dbc6cc1b8
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-11-17 10:24:24 -0300

    add char_bat

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
 import 'package:flutter/material.dart';
@@ -51,20 +52,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [];
-  //   Transaction(
-  //     id: 't1',
-  //     title: 'Novo Tênis de Corrida',
-  //     value: 310.76,
-  //     date: DateTime.now(),
-  //   ),
-  //   Transaction(
-  //     id: 't2',
-  //     title: 'Conta de Luz',
-  //     value: 211.30,
-  //     date: DateTime.now(),
-  //   ),
-  // ];
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta Antiga',
+      value: 500,
+      date: DateTime.now().subtract(Duration(days: 30)),
+    ),
+  ];
+
+
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((tr){
+      return tr.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      
+      ));
+    }).toList();
+  }
 
    _addTransaction(String title, double value){
     final newTransaction = Transaction(
@@ -109,13 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              child: Card(
-                color: Colors.blue,
-                child: Text('Gráfico'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_transactions),
           ],
         ),
